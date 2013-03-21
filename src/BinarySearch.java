7ac61ffa22248539f4f3947fb337e62fdf178cc7
Peter Fitzgibbons <pfitzgibbons@gmail.com>
2013-03-21 08:17:39 -0500

    Init

---
+++ src/BinarySearch.java
@@ -0,0 +1,34 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pfitzgibbons
+ * Date: 3/21/13
+ * Time: 7:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static int rank(int key, int[] a)
+    {  // Array must be sorted.
+        int lo  = 0;
+        int hi = a.length - 1;
+        while (lo <= hi)
+        {  // Key is in a[lo..hi] or not present.
+            int mid = lo + (hi - lo) / 2;
+            if      (key < a[mid]) hi = mid - 1;
+            else if (key > a[mid]) lo = mid + 1;
+            else                   return mid;
+        }
+        return -1; }
+    public static void main(String[] args)
+    {
+        int[] whitelist = In.readInts(args[0]);
+        Arrays.sort(whitelist);
+        while (!StdIn.isEmpty())
+        {  // Read key, print if not in whitelist.
+            int key = StdIn.readInt();
+            if (rank(key, whitelist) < 0)
+                StdOut.println(key);
+        }
+    }
+}
